a9752e0f8e6dc128bafebcfb537b2b848589c2c5
Tinotenda Jobi <tinojobi@gmail.com>
2021-10-14 15:49:41 +0200

    added actuator

---
+++ movie-catalog-service/src/main/java/io/jobi/moviecatalogservice/resources/PropertiesController.java
@@ -0,0 +1,21 @@
+package io.jobi.moviecatalogservice.resources;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RefreshScope
+@Slf4j
+public class PropertiesController {
+    @Value("${my.property:property}")
+    private String property;
+
+    @GetMapping("/property")
+    public String getProperty(){
+        return property;
+    }
+
+}
